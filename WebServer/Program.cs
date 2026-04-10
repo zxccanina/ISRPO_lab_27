9615abb809fb2ac6f5bdd984fc063fa864499d7c
ВашÐcokes238 <_cokes238@gmail.com>
2026-04-10 11:39:41 +0300

    sam reshu

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -11,4 +11,25 @@ app.MapGet("/hello/{name}", (string name) => $"hi, {name}!");
 
 app.MapGet("/sum/{a}/{b}", (int a, int b) => $"sum = {a + b}");
 
+app.MapGet("/student", () => new {
+    name = "Gena",
+    group = "ISP - 231",
+    year = 3,
+    IsActive = true
+});
+
+app.MapGet("/subjects", () => new[] {
+    1,
+    2,
+    3,
+    4
+});
 app.Run();
+
+app.MapGet("/product/{Id}", (int id) => new Product(
+    Id: id,
+    Name: $"Товар #{id}",
+    Price: id * 99.99m,
+    InStock: id % 2 == 0
+));
+record Product(int Id, string Name, decimal Price, bool InStock);
